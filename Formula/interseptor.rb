6b43213ed0020cbaee9de0396e8eac70e801d522
des <des@local>
2026-07-17 11:12:56 +0700

    interseptor 1.5.2

--- Formula/interseptor.rb
+++ Formula/interseptor.rb
@@ -2,15 +2,15 @@ class Interseptor < Formula
   desc "Intercepting HTTP/HTTPS proxy + security toolkit (single static Go binary)"
   homepage "https://github.com/Veyal/interseptor"
   license "MIT"
-  version "1.5.1"
+  version "1.5.2"
 
   on_arm do
-    url "https://github.com/Veyal/interseptor/releases/download/v1.5.1/interseptor_1.5.1_darwin_arm64.tar.gz"
-    sha256 "e54a5821c091a30e5a5e8b503b91e945d8a00f08126cb85abe629d6854a26b26"
+    url "https://github.com/Veyal/interseptor/releases/download/v1.5.2/interseptor_1.5.2_darwin_arm64.tar.gz"
+    sha256 "639e4499b20fb77090c098261794ca70ec4d33446d1a1fd887ef02feaa5d606d"
   end
   on_intel do
-    url "https://github.com/Veyal/interseptor/releases/download/v1.5.1/interseptor_1.5.1_darwin_amd64.tar.gz"
-    sha256 "26a4c46676cf3016be5159b6dc8db9124dd662c358aac8f3cfe58af3183855ee"
+    url "https://github.com/Veyal/interseptor/releases/download/v1.5.2/interseptor_1.5.2_darwin_amd64.tar.gz"
+    sha256 "44bffaa769fb10b51aa7c14397b74aace2eb13a5abcdb180c480bf4fce92b965"
   end
 
   def install
